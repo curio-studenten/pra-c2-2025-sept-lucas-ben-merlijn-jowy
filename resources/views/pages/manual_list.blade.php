8652c6497cfd8dd9e75c4c27f8b7cf7227fb756e
hazeliciousness <happynijman@gmail.com>
2025-09-18 11:18:23 +0200

    Ticket 5

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -5,25 +5,40 @@
     </x-slot:head>
 
     <x-slot:breadcrumb>
-        <li><a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/" alt="Manuals for '{{$brand->name}}'" title="Manuals for '{{$brand->name}}'">{{ $brand->name }}</a></li>
+        <li>
+            <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/"
+               alt="Manuals for '{{ $brand->name }}'"
+               title="Manuals for '{{ $brand->name }}'">
+                {{ $brand->name }}
+            </a>
+        </li>
     </x-slot:breadcrumb>
-
 
     <h1>{{ $brand->name }}</h1>
 
     <p>{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
 
-
+    <div style="display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); gap: 0.5rem;">
         @foreach ($manuals as $manual)
-
-            @if ($manual->locally_available)
-                <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-                ({{$manual->filesize_human_readable}})
-            @else
-                <a href="{{ $manual->url }}" target="new" alt="{{ $manual->name }}" title="{{ $manual->name }}">{{ $manual->name }}</a>
-            @endif
-
-            <br />
+            <div style="border: 1px solid #ccc; border-radius: 4px; padding: 6px; text-align: center;">
+                @if ($manual->locally_available)
+                    <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/{{ $manual->id }}/"
+                       alt="{{ $manual->name }}"
+                       title="{{ $manual->name }}">
+                        {{ $manual->name }}
+                    </a>
+                    <div style="font-size: 0.85em; color: #666;">
+                        ({{ $manual->filesize_human_readable }})
+                    </div>
+                @else
+                    <a href="{{ $manual->url }}" target="new"
+                       alt="{{ $manual->name }}"
+                       title="{{ $manual->name }}">
+                        {{ $manual->name }}
+                    </a>
+                @endif
+            </div>
         @endforeach
+    </div>
 
 </x-layouts.app>
